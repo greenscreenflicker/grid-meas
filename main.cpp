5b1b440d2ed3df20ce30626ee6cb72a7c7d57f56
Michael Heidinger <mch dot heidinger aaaatttttttt gmx dot germany>
2016-11-28 18:03:51 +0100

    Implemented ringbuffer, now correctly. Maybe some swing-in-optimisation?

--- main.cpp
+++ main.cpp
@@ -112,16 +112,16 @@ double Fmeas (double s_0, double fsample)   // 2.2 Frequency Measurement
 
 }
 
-#define NUM_OF_AVG 1000
+#define NUM_OF_AVG 20
 #define NUM_OF_MEM (5+NUM_OF_AVG)
 
 double sample[NUM_OF_MEM];
-double Fmeas_uniavg (double s_0, double fsample)   // 2.2 Frequency Measurement
+float Fmeas_uniavg (float s_0, float fsample)   // 2.2 Frequency Measurement
 {
   double K = 2.0;
 
-  double LS1=0;
-  double LS2=0;
+  float LS1=0;
+  float LS2=0;
   sample[0]=s_0;
   int i;
   for(i=0;i<NUM_OF_AVG;i++){
@@ -133,66 +133,62 @@ double Fmeas_uniavg (double s_0, double fsample)   // 2.2 Frequency Measurement
 
   //fprintf(fp,"%10f %10.8f %10.8f %10.8f %10f %10f %10f %10f %10f %10f\n",f, (LS2/(2*LS1)), LS1, LS2, sample[0],sample[1],sample[2],sample[3],sample[4],amplitude);
   
-  for(i=(NUM_OF_MEM-1);i>0;i--){
-    sample[i]=sample[i-1];
-  }
-  
+
+	for(i=(NUM_OF_MEM-1);i>0;i--){
+		sample[i]=sample[i-1];
+	}
+
   return f;
 
 
 }
 
+int map_position(int rbp, int shift, int i){
+	int map=rbp-(5-shift+i);
+	if(map<0)map=map+NUM_OF_MEM;
+//	printf("m(%i)",map);
+	return map;
+}
+
+
+int fm(int v){
+	if(v<0){
+		v=v+NUM_OF_MEM;
+	}
+	return v;
+}
 
 int ringbuf_position=0;
 float grid_sample[NUM_OF_MEM];
 float Fmeas_ringbuffer(float s_0, float fsample){
-	double LS1=0;
-	double LS2=0;
-	double K = 2.0;
-	grid_sample[ringbuf_position]=s_0;
 	if(ringbuf_position>(NUM_OF_MEM-1)){
 		ringbuf_position=0;
 	}
+	printf("f:%i\n",ringbuf_position);
+	grid_sample[ringbuf_position]=s_0;
+
+
+	double K = 2.0;
+
+	float LS1=0;
+	float LS2=0;
+	double f;
 	int i;
-	for(i=1;i<(NUM_OF_AVG-1);i++){
-		LS1=LS1+(sample[5-0+i]*sample[5-3+i]-sample[5-1+i]*sample[5-2+i]);
-		LS2=LS2+(sample[5-0+i]*sample[5-5+i]-sample[5-1+i]*sample[5-4+i]);
-	}
-	/*
-	int left=ringbuf_position;
-	printf("left: %i\n",ringbuf_position);
-	switch(left){
-		default:
-		case 5:
-			i=NUM_OF_AVG+4;
-			LS1=LS1+(sample[0+i]*sample[2]-sample[0]*sample[1]);
-			LS2=LS2+(sample[0+i]*sample[4]-sample[0]*sample[3]);	
-		case 4:
-			i=NUM_OF_AVG+3;
-			LS1=LS1+(sample[0+i]*sample[1]-sample[1+i]*sample[0]);
-			LS2=LS2+(sample[0+i]*sample[3]-sample[1+i]*sample[2]);			
-		case 3:
-			i=NUM_OF_AVG+2;
-			LS1=LS1+(sample[0+i]*sample[0]-sample[1+i]*sample[2+i]);
-			LS2=LS2+(sample[0+i]*sample[2]-sample[1+i]*sample[1]);		
-		case 2:
-			i=NUM_OF_AVG+1;
-			LS1=LS1+(sample[0+i]*sample[3+i]-sample[1+i]*sample[2+i]);
-			LS2=LS2+(sample[0+i]*sample[1]-sample[1+i]*sample[0]);	
-		case 1:
-			i=NUM_OF_AVG;
-			LS1=LS1+(sample[0+i]*sample[3+i]-sample[1+i]*sample[2+i]);
-			LS2=LS2+(sample[0+i]*sample[0]-sample[1+i]*sample[4+i]);
-			break;
-		case 0:
-			printf("nix");
-			break;
-	}
-	*/
 	
-	double f = (fsample/(TWOPI * K)) *acos(LS2/(2*LS1));
-	ringbuf_position++;
+	for(i=0;i<NUM_OF_AVG;i++){
+		LS1=LS1+(grid_sample[fm(-0+ringbuf_position-i)]*grid_sample[fm(-3+ringbuf_position-i)]-grid_sample[fm(-1+ringbuf_position-i)]*grid_sample[fm(-2+ringbuf_position-i)]);
+		LS2=LS2+(grid_sample[fm(-0+ringbuf_position-i)]*grid_sample[fm(-5+ringbuf_position-i)]-grid_sample[fm(-1+ringbuf_position-i)]*grid_sample[fm(-4+ringbuf_position-i)]);
+		printf("LS1:%3i*%3i+%3i*%3i\n",fm(ringbuf_position-i),fm(ringbuf_position-3-i),fm(ringbuf_position-1-i),fm(ringbuf_position-2-i));
+		printf("LS2:%3i*%3i+%3i*%3i\n",fm(ringbuf_position-i),fm(ringbuf_position-5-i),fm(ringbuf_position-1-i),fm(ringbuf_position-4-i));
+	}
+	f = (fsample/(TWOPI * K)) *acos(LS2/(2*LS1));
+	//fprintf(fp,"%10f %10.8f %10.8f %10.8f %10f %10f %10f %10f %10f %10f\n",f, (LS2/(2*LS1)), LS1, LS2, sample[0],sample[1],sample[2],sample[3],sample[4],amplitude);
+
+
+	ringbuf_position=ringbuf_position+1;
+
 	return f;
+  
 }
 
 
@@ -241,7 +237,7 @@ void analysePhasor()
     for (int n = 0; n <= 2000; n++)
     {
         // Signal
-        double x = amp*(sin(  n * W )+ 1000*(rand()%1000)*0.000000001);
+        double x = amp*(sin(  n * W ));//+ 1000*(rand()%1000)*0.000000001);
         double xref = sin(  n * W_ref);
         
 		f = Fmeas_ringbuffer(x,fsample);
@@ -261,7 +257,7 @@ void analysePhasor()
 
 		double f_unifilter=Fmeas_uniavg(x,fsample);
         // Print Result(s)
-        printf("%5i %10.2f   %10f   o:%-10f r:%-10f A:%f\n",n, x, w_deg,f_unifilter,f,amplitude);
+        printf("%5i %10.2f   %10f   o:%-10f r:%-10f A:%f\n",n%NUM_OF_MEM, x, w_deg,f_unifilter,f,amplitude);
 
     }
     fclose(fp);
